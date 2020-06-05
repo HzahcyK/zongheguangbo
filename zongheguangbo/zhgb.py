fd57d43c77af5278275ec93411487efe0e931a00
HzahcyK <zhy547155267@qq.com>
2020-06-05 14:20:29 +0800

    zhgb

--- zongheguangbo/zhgb.py
+++ zongheguangbo/zhgb.py
@@ -4,24 +4,33 @@ import xlsxwriter
 start_n = input("请输入查询起始月份：")
 end_n = input("请输入查询截止月份：")
 start_time = '2020-0' + start_n + '-01 00:00:00'
-end_time = '2020-0' + end_n + '01 00:00:00'
+end_time = '2020-0' + end_n + '-01 00:00:00'
 file_name = input("文件名：")
 conn = pymysql.connect(host="10.10.10.240", port=5432, user="root", password="tF!e5UN?iGMRkB7Z80Ln#O@uCsP^mS", db="dj_analytics", charset="utf8")
 cursor = conn.cursor()
-cursor.execute("select pub_date, title, case is_original when 0 then '转载' when 1 then '原创' else 'null' end original, category, editor, read_count, department from views_article_department_channel where department='综合广播' and (pub_date between '%s' and '%s');" % (start_time, end_time))
+cursor.execute("select （@i:=@i+1) id, pub_date, title, case is_original when 0 then '转载' when 1 then '原创' else 'null' end original, category, editor, read_count, department from views_article_department_channel, (select @i:=0) r where department='综合广播' and (pub_date between '%s' and '%s');" % (start_time, end_time))
 result = cursor.fetchall()
 result = list(result)
 datas = []
 for item in result:
-    item = [str(item[0]), item[1],  item[2], item[3],  item[4], item[5], item[6]]
+    item = [item[0], str(item[1]),  item[2], item[3],  item[4], item[5], item[6], item[7]]
     datas.append(item)
 print(datas)
 
 workbook = xlsxwriter.Workbook(file_name + '.xlsx')
 worksheet = workbook.add_worksheet()
-for i in range(1, len(datas)):
+worksheet.set_column("B:B", 21)
+worksheet.set_column("C:C", 70)
+bold = workbook.add_format({
+    'bold': 1,             #字体加粗
+    'fg_color': 'green',   #单元格背景颜色
+    'align': 'center',     #对齐方式
+    'valign': 'vcenter',   #字体对齐方式
+ })
+title = [u'id', u'发布时间', u'标题', u'是否原创', u'栏目', u'编辑', u'阅读量', u'部门']
+worksheet.write_row('A1', title, bold)
+for i in range(2, len(datas)):
         row = 'A' + str(i)
         worksheet.write_row(row, datas[i-1])
-        break
 workbook.close()
 
