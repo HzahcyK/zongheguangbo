613a7cefcf561cb0f88a4d2bf8da64e90966a603
HzahcyK <zhy547155267@qq.com>
2020-06-05 14:24:45 +0800

    zhgb

--- zongheguangbo/zhgb.py
+++ zongheguangbo/zhgb.py
@@ -8,7 +8,7 @@ end_time = '2020-0' + end_n + '-01 00:00:00'
 file_name = input("文件名：")
 conn = pymysql.connect(host="10.10.10.240", port=5432, user="root", password="tF!e5UN?iGMRkB7Z80Ln#O@uCsP^mS", db="dj_analytics", charset="utf8")
 cursor = conn.cursor()
-cursor.execute("select （@i:=@i+1) id, pub_date, title, case is_original when 0 then '转载' when 1 then '原创' else 'null' end original, category, editor, read_count, department from views_article_department_channel, (select @i:=0) r where department='综合广播' and (pub_date between '%s' and '%s');" % (start_time, end_time))
+cursor.execute("select (@i:=@i+1) id, pub_date, title, case is_original when 0 then '转载' when 1 then '原创' else 'null' end original, category, editor, read_count, department from views_article_department_channel, (select @i:=0) r where department='综合广播' and (pub_date between '%s' and '%s');" % (start_time, end_time))
 result = cursor.fetchall()
 result = list(result)
 datas = []
