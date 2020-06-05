91b47dc432e7e4ebe22ede2e6c517169abd75f2b
HzahcyK <zhy547155267@qq.com>
2020-06-05 15:15:38 +0800

    zhgb

--- zongheguangbo/zhgb.py
+++ zongheguangbo/zhgb.py
@@ -4,8 +4,8 @@ import xlsxwriter
 year = input("请输入查询年份：")
 start_n = input("请输入查询起始月份：")
 end_n = input("请输入查询截止月份：")
-start_time = '2020-' + start_n + '-01 00:00:00'
-end_time = '2020-' + end_n + '-01 00:00:00'
+start_time = year + '-' + start_n + '-01 00:00:00'
+end_time = year + '-' + end_n + '-01 00:00:00'
 file_name = year + '-' + start_n + '~' + end_n
 conn = pymysql.connect(host="10.10.10.240", port=5432, user="root", password="tF!e5UN?iGMRkB7Z80Ln#O@uCsP^mS", db="dj_analytics", charset="utf8")
 cursor = conn.cursor()
@@ -20,7 +20,7 @@ print(datas)
 
 workbook = xlsxwriter.Workbook(file_name + '.xlsx')
 worksheet = workbook.add_worksheet()
-title_style = workbook.add_format({
+colname_style = workbook.add_format({
     'bold': 1,             #字体加粗
     'fg_color': 'yellow',   #单元格背景颜色
     'align': 'center',     #对齐方式
@@ -33,13 +33,18 @@ id_style = workbook.add_format({
 content_style = workbook.add_format({
     'align': 'center',
 })
+title_style = workbook.add_format({
+    'align': 'left',
+})
 worksheet.set_column("A:A", None, id_style)
 worksheet.set_column("B:B", 21)
 worksheet.set_column("C:C", 70)
 title = [u'id', u'发布时间', u'标题', u'是否原创', u'栏目', u'编辑', u'阅读量', u'部门']
-worksheet.write_row('A1', title, title_style)
+worksheet.write_row('A1', title, colname_style)
 for i in range(2, len(datas)):
         row = 'A' + str(i)
         worksheet.write_row(row, datas[i-2], content_style)
+        worksheet.set_column("A:A", None, id_style)
+        worksheet.set_column("C:C", None, title_style)
 workbook.close()
 
